b51dfef6cd4b1a1a92b316ffd538b26d52eff868
Vitor Oliveira <127554215+Vitorfran@users.noreply.github.com>
2024-08-16 17:19:29 -0300

    community

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main{
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("OLa");
+        System.out.println("OLA BOVOES");
     }
 }
