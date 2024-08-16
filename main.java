3dd083e115c2a578dcd8a24c695d5773d9473360
Vitor Oliveira <127554215+Vitorfran@users.noreply.github.com>
2024-08-16 17:22:43 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("OLa");
-        System.out.println("OLA BOVOES");
+      int a = 3;
+
     }
 }
